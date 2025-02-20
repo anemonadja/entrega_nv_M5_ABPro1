5085e2c9ae79b7edef9b4696bd3ca4f6003e4145
anemonadja <nadja.villarroel.n@gmail.com>
2025-02-20 01:49:39 -0300

    cambios footer

--- hospitalReact1/src/components/Footer.jsx
+++ hospitalReact1/src/components/Footer.jsx
@@ -4,17 +4,20 @@ import Card from 'react-bootstrap/Card';
 function FooterReact() {
     return (
         <Card className="text-center">
-            <Card.Header></Card.Header>
             <Card.Body>
-                <Card.Title>Footer</Card.Title>
-                <Card.Text>
-                    Información Especial
-                </Card.Text>
-                <Button variant="primary">Ver mapa</Button>
+                <footer class="py-1 my-1">
+                    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li class="nav-item"><a href="./home" class="nav-link px-2 text-body-secondary">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+                    <li class="nav-item"><a href="https://youtu.be/T_QAxvPjjBc?si=2dsuBsWl6BgqRFz2" Target="_blank" class="nav-link px-2 text-body-secondary">Video</a></li>
+                    <li class="nav-item"><a href="./doctores" class="nav-link px-2 text-body-secondary">Doctores</a></li>
+                    <li class="nav-item"><a href="./contacto" class="nav-link px-2 text-body-secondary">Contacto</a></li>
+                    </ul>
+                    <p class="text-center text-body-secondary">© 2025 Hospital React_Nadja</p>
+                </footer>
             </Card.Body>
-            <Card.Footer className="text-muted"></Card.Footer>
         </Card>
     );
-  }
-  
-  export default FooterReact;
+}
+
+    export default FooterReact;
